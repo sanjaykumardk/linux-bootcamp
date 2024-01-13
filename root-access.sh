ea90981e3a4b9954b02d46a537869fb7e0140afb
SANJAY KUMAR D K <145103929+sanjaykumardk@users.noreply.github.com>
2024-01-13 17:20:33 +0530

    Create root-access.sh

---
+++ root-access.sh
@@ -0,0 +1,18 @@
+##########################
+## Running commands as root (sudo, su)
+##########################
+ 
+# running a command as root (only users that belong to sudo group [Ubuntu] or wheel [CentOS])
+sudo command
+ 
+# becoming root temporarily in the terminal
+sudo su      # => enter the user's password
+ 
+# setting the root password
+sudo passwd root
+ 
+# changing a user's password
+passwd username
+ 
+# becoming root temporarily in the terminal
+su     # => enter the root password
