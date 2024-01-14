901447e94eed5036f46e6016104f0b1d7cc776d6
SANJAY KUMAR D K <145103929+sanjaykumardk@users.noreply.github.com>
2024-01-14 09:37:38 +0530

    Create linux-paths.sh

---
+++ linux-paths.sh
@@ -0,0 +1,20 @@
+##########################
+## Linux Paths
+##########################
+ 
+.       # => the current working directory
+..      # => the parent directory
+~       # => the user's home directory
+ 
+cd      # => changing the current directory to user's home directory
+cd ~    # => changing the current directory to user's home directory
+cd -    # => changing the current directory to the last directory
+cd /path_to_dir    # => changing the current directory to path_to_dir 
+pwd     # => printing the current working directory
+ 
+# installing tree
+sudo apt install tree
+ 
+tree directory/     # => Ex: tree .
+tree -d .           # => prints only directories
+tree -f .           # => prints absolute paths
